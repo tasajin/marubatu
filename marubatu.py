317eec4ad7dce3731ba5657d4683b5910567b35a
tasajin <132569473+tasajin@users.noreply.github.com>
2023-05-24 23:43:38 +0900

    start

---
+++ marubatu.py
@@ -0,0 +1,11 @@
+import tkinter
+
+class marubatu():
+    def __init__(self, size=5):
+        self.size = size
+        self.app = tkinker.Tk()
+        self.app.title("〇×ゲーム")
+        self.turn = ("〇の番です！")
+
+        
+        pass
