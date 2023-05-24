f17ac43ffc85633a720a0950483475722e47969e
tasajin <132569473+tasajin@users.noreply.github.com>
2023-05-25 03:59:06 +0900

    升目の設定

--- marubatu.py
+++ marubatu.py
@@ -1,11 +1,44 @@
-import tkinter
+from tkinter import *
+from tkinter import ttk
 
-class marubatu():
-    def __init__(self, size=5):
-        self.size = size
-        self.app = tkinker.Tk()
-        self.app.title("〇×ゲーム")
-        self.turn = ("〇の番です！")
+
+squaers = 3
+
+class TictacApp(ttk,Frame):
+    def __init__(self, master=None):
+        super()._init__(master)
+        self.button_masu()
+
+
+    def button_masu(self):
+        for i in range(squares):
+            for j in range(squaers):
+                button = ttk.Button(self,command=self.record(i,j))
+                button.bind('<Button-1>', self.mark)
+                button.grid(column=i, row=j, sticky=(N, S, E, W))
+
+            for i in range(squaers):
+                self.columnconfigure(i, weight=1)
+                self.rowconfigure(i,weight=1)
+
+            self.master.columnconfigure(0, weight=1)
+            self.master.rowconfigure(0,weight=1)
+
+            self.grid(column=0, row=0, sticky(N, S, E, W))
+
+
+
+
+
+
+#ウィンドウを表示し続ける？
+def main():
+    root = Tk()
+    root.title('目並べ、〇×ゲーム')
+    TictapApp(root)
+    root.mainloop()
+
+if __name__ == ' __main__ ':
+    main()
 
         
-        pass
