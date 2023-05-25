38805f04dbbed3eff7131abbd7e766542f6a30bc
tasajin <132569473+tasajin@users.noreply.github.com>
2023-05-25 12:12:37 +0900

    交代できたが、勝敗の結果が出ない。

--- marubatu.py
+++ marubatu.py
@@ -1,4 +1,3 @@
-
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
@@ -14,7 +13,7 @@ class TictacApp(ttk.Frame):
     def button_masu(self):
         for i in range(squares):
             for j in range(squares):
-                button = ttk.Button(self, command=lambda i=i, j=j:　self.record(i, j))
+                button = ttk.Button(self, command=lambda i=i, j=j: self.record(i, j))
                 button.bind('<Button-1>', self.mark)
                 button.grid(column=i, row=j, sticky=(N, S, E, W))
 
@@ -23,33 +22,34 @@ class TictacApp(ttk.Frame):
             self.rowconfigure(i, weight=1)
 
         self.master.columnconfigure(0, weight=1)
-        self.master.rowconfigure(0,weight=1)
+        self.master.rowconfigure(0, weight=1)
 
-        self.grid(column=0, row=0, sticky(N, S, E, W))
+        self.grid(column=0, row=0, sticky=(N, S, E, W))
 
     def mark(self, event):
         if not event.widget['text']:
             if self.player == 1:
-                event.widget['text'] = str('〇')
+                event.widget['text'] = '〇'
             else:
-                event.widget['text'] = str('×')
+                event.widget['text'] = '×'
+            self.turn()
 
     def value(self):
         self.player = 1
         self.field = []
-        for i in range(squares):
-            self.field.appnd(['' for i in range(squares)])
+        for t in range(squares): 
+            self.field.append(['' for t in range(squares)])
         self.finish = 0
 
-    def record(self,i,j):
+    def record(self, i, j):
         def x():
             if not self.field[i][j]:
                 self.field[i][j] = self.player
                 self.winner_check()
                 self.turn()
                 self.clear()
-            return x
-        
+        return x
+
     def winner_check(self):
         naname = 0
         for i in range(squares):
@@ -60,9 +60,9 @@ class TictacApp(ttk.Frame):
                     yoko += 1
                 if self.field[j][i] == self.player:
                     tate += 1
-            if self.field[i][j] == self.player:
+            if self.field[i][i] == self.player:
                 naname += 1
-            if yoko ==3 or tate == 3 or naname == 3:
+            if yoko == 3 or tate == 3 or naname == 3:
                 self.result()
         if self.field[0][2] == self.field[1][1] == self.field[2][0] == self.player:
             self.result()
@@ -73,14 +73,14 @@ class TictacApp(ttk.Frame):
 
     def result(self):
         if self.player == 1:
-            messagebox.showinfo('〇のプレイヤーの勝利！！')
+            messagebox.showinfo('〇のプレイヤーの勝利！')
         else:
-            messagebox.showinfo('×のプレイヤーの勝利！！')
+            messagebox.showinfo('×のプレイヤーの勝利！')
         self.finish = 1
 
     def clear(self):
         if self.finish == 1:
-            self.button_masu()
+            self.button_masu()  
             self.value()
 
 
@@ -92,5 +92,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-        
